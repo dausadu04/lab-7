0b3d6ea5e3aee508b444f86ce2cc4f67eefbe955
dausadu04 <dausadu15@gmail.com>
2023-11-07 18:07:41 +0600

    main

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import random
+import re
 
 lists = []
 
-# with open("dauletsuper.txt", 'r') as files:
-#     for line in files:
-#         words = line.strip().split()
-#         lists.extend(words)
+with open("dauletsuper.txt", 'r') as files:
+    for line in files:
+        words = line.strip().split()
+        lists.extend(words)
 
 
 def task_1():
@@ -35,11 +36,132 @@ def task_3():
 
 def task_4():
     count = 0
-    with open('boring_file.txt', 'r') as fil:
-        for line in fil:
-            words = line.strip().split()
-            if words and words[0].lower() == 'd':
-                count += 1
-                print(f"{words}\n{count}")
+    with open('boring_file.txt', 'r') as file:
+        for i in file:
+            i = i.strip().split()
+            for j in i:
+                if j and j[0].lower() == 'd':
+                    count += 1
+                    print(f"{j}\n{count}")
 
-task_4()
+
+# task_4()
+
+
+def task_5():
+    count = 0
+    with open('boring_file.txt', 'r') as file:
+        for i in file:
+            i = i.strip().split()
+            for j in i:
+                if j and j[-1].lower() == 'f':
+                    count += 1
+                    print(f"{j}")
+
+        print(count)
+
+
+# task_5()
+
+def task_6():
+    count = 0
+    with open('chatgpt_text.txt', 'r') as file:
+        for i in file:
+            i = i.strip().split()
+            for j in i:
+                if j == 'all' or j == 'none':
+                    count += 1
+        print(count)
+
+
+# task_6()
+
+
+def task_6_2():
+    with open('chatgpt_text.txt', 'r') as file:
+        file = file.read()
+        count_all = file.count(' all ')
+        count_none = file.count(' none ')
+        counts = count_none + count_all
+    print(counts)
+
+
+# task_6_2()
+
+def task_7():
+    checked = set()
+    lists = []
+    with open('chatgpt_text.txt', 'r') as file:
+        for line in file:
+            words = line.strip().split()
+            lists.extend(words)
+
+    for i in lists:
+        i = i.strip()
+        count = lists.count(i)
+        if (i, count) not in checked:
+            checked.add((i, count))
+    print(checked)
+
+
+# task_7()
+
+def task_8():
+    max_length = 0
+    with open('chatgpt_text.txt', 'r') as file:
+        for i in file:
+            i = i.strip().split()
+            for j in i:
+                if max_length < len(j):
+                    max_length = len(j)
+                    word = j
+    print(f"{max_length}\n{word}")
+
+
+# task_8()
+
+def task_8_2():
+    lists = []
+
+    with open('chatgpt_text.txt', 'r') as file:
+        for i in file:
+            i = i.strip().split()
+            lists.extend(i)
+    print(max(lists, key=len))
+
+
+# task_8_2()
+
+def task_9():
+    with open('gay boy', 'r+') as file:
+        content = file.read()
+        content = content.replace('b', 'j')
+
+        file.seek(0)
+        file.write(content)
+        file.truncate()
+
+        print(content)
+
+
+# task_9()
+
+def task_10():
+    # gay boy is {I am a joy.}
+    checked = set()
+    with open('gay boy', 'r') as file:
+        lines = file.read()
+        for i in lines:
+            i = i.split()
+            for j in i:
+                j = j.lower().strip()
+                for k in j:
+                    if k not in checked:
+                        count = i.count(k)
+                        if count == 0:
+                            count += 1
+                        checked.add((k, count))
+    print(checked)
+
+
+# task_10()
